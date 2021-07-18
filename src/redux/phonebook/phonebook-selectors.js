5ce36e4c574407a699659e285ca91a7059f0bfe1
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-07-18 17:36:26 +0300

    Memo selector to get visible contacts

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -1,13 +1,25 @@
+import { createSelector } from 'reselect';
+
 export const getAdd = state => state.phonebook.contacts;
 
 export const getFilter = state => state.phonebook.filter;
 
-export const getVisibleContacts = state => {
-  const contacts = getAdd(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
+// export const getVisibleContacts = state => {
+//   const contacts = getAdd(state);
+//   const filter = getFilter(state);
+//   const normalizedFilter = filter.toLowerCase();
 
-  return contacts.filter(({ name }) => {
-    return name.toLowerCase().includes(normalizedFilter);
-  });
-};
+//   return contacts.filter(({ name }) => {
+//     return name.toLowerCase().includes(normalizedFilter);
+//   });
+// };
+
+export const getVisibleContacts = createSelector(
+  [getAdd, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(normalizedFilter);
+    });
+  },
+);
